b2e20016cf6a576aec163ba122b2eedab4be5693
simz007 <51645428+simz007@users.noreply.github.com>
2019-10-10 12:50:16 -0400

    Project1: Change the game visuals
    -Added images for the Player and prey
    -Added background Image
    - Added functions to display the score and the player's health

--- projects/Project01/js/script.js
+++ projects/Project01/js/script.js
@@ -2,8 +2,8 @@
 
 /******************************************************
 
-Game - Chaser
-Pippin Barr
+Fish O Fish
+Simon Zogheib
 
 A "simple" game of cat and mouse. The player is a circle and can move with keys,
 if they overlap the (randomly moving) prey they "eat it" by sucking out its life
@@ -15,13 +15,14 @@ random movement, screen wrap.
 
 ******************************************************/
 
+
 // Track whether the game is over
 let gameOver = false;
 
 // Player position, size, velocity
 let playerX;
 let playerY;
-let playerRadius = 25;
+let playerRadius = 35;
 // New variable for Eating preyRadius
 let playerEatingRadius = 0.5;
 
@@ -33,9 +34,9 @@ let playerVY = 0;
 let playerMaxSpeed = 3;
 // Player health
 let playerHealth;
-let playerMaxHealth = 255;
+let playerMaxHealth = 900;
 // Player fill color
-let playerFill = 50;
+let playerFill = 50 ;
 
 // Create variables for player sprint speed and player initial speed
 let playerSprintSpeed = 5;
@@ -45,17 +46,24 @@ let playerSprintSpeed = 5;
 let playerInitialSpeed = 3;
 
 // Create the variable for player's speed decrease after eating a prey
-let playerDecreaseSpeed = 0.05;
+let playerDecreaseSpeed = 0.03;
 
 // Create variable for player health loss
 let playerHealthLoss = 3;
+
+// Player and prey images
+let playerImage;
+let preyImage;
+
+// background image
+let bgImage;
 
 
 
 // Prey position, size, velocity
 let preyX;
 let preyY;
-let preyRadius = 25;
+let preyRadius = 15;
 let preyVX;
 let preyVY;
 
@@ -76,11 +84,27 @@ let preyEaten = 0;
 let tx;
 let ty;
 
+// Add variables for the sounds
+let waterSound;
+let eatSound;
+
+
+
+function preload() {
+  // Preload my images
+  playerImage = loadImage("assets/images/puff.png");
+  preyImage = loadImage("assets/images/prey.png");
+  bgImage = loadImage("assets/images/bg1.png");
+
+  // Preload my sounds
+
+}
+
 // setup()
 //
 // Sets up the basic elements of the game
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(700, 500);
 
   noStroke();
 
@@ -121,7 +145,13 @@ function setupPlayer() {
 // displays the two agents.
 // When the game is over, shows the game over screen.
 function draw() {
-  background(100, 100, 200);
+
+  // Add the Background image
+  image(bgImage, 0, 0);
+
+
+
+
 
   if (!gameOver) {
     handleInput();
@@ -134,6 +164,10 @@ function draw() {
 
     drawPrey();
     drawPlayer();
+
+    showScore();
+    showHealth();
+
   } else {
     showGameOver();
   }
@@ -239,7 +273,7 @@ function checkEating() {
     // Increase the player's radius after eating a prey
     playerRadius += playerEatingRadius;
     // Constrain the player radisu size so it won't get too big
-    playerRadius = constrain(playerRadius, playerRadius, 30);
+    playerRadius = constrain(playerRadius, playerRadius, 40);
 
     // Decrease the player's speed after eating a prey
     playerInitialSpeed -= playerDecreaseSpeed;
@@ -255,8 +289,10 @@ function checkEating() {
       preyHealth = preyMaxHealth;
       // Track how many prey were eaten
       preyEaten = preyEaten + 1;
-
     }
+
+
+
   }
 }
 
@@ -306,16 +342,23 @@ function movePrey() {
 //
 // Draw the prey as an ellipse with alpha based on health
 function drawPrey() {
+  push();
   fill(preyFill, preyHealth);
-  ellipse(preyX, preyY, preyRadius * 2);
+  imageMode(CENTER);
+  image(preyImage, preyX, preyY, 30, 30);
+  pop();
 }
 
 // drawPlayer()
 //
 // Draw the player as an ellipse with alpha value based on health
 function drawPlayer() {
-  fill(playerFill, playerHealth);
-  ellipse(playerX, playerY, playerRadius * 2);
+push();
+  tint(255, playerHealth);
+  imageMode(CENTER);
+  image(playerImage, playerX, playerY, playerRadius*2, playerRadius*2);
+  pop();
+
 }
 
 // showGameOver()
@@ -327,9 +370,28 @@ function showGameOver() {
   textAlign(CENTER, CENTER);
   fill(0);
   // Set up the text to display
-  let gameOverText = "GAME OVER\n"; // \n means "new line"
-  gameOverText = gameOverText + "You ate " + preyEaten + " prey\n";
-  gameOverText = gameOverText + "before you died."
+  let gameOverText = "GAME OVER PUFFY\n"; // \n means "new line"
+  gameOverText = gameOverText + "You ate " + preyEaten + " fishes\n";
+  gameOverText = gameOverText + "Swim faster next time!."
   // Display it in the centre of the screen
   text(gameOverText, width / 2, height / 2);
 }
+
+
+// Function that Displays the player's health
+  function showHealth() {
+ textFont("Helvetica");
+  textAlign(LEFT, TOP);
+  textSize(20);
+  fill(255);
+  text("Puff Health: " + playerHealth, 10, 10);
+}
+
+// Function that Displays the ammount of fish eaten
+    function showScore() {
+    textFont("Helvetica");
+    textAlign(LEFT, TOP);
+    textSize(20);
+    fill(255);
+    text("Fish Eaten: " + preyEaten, 10, 35);
+  }
