98ae6c77afdec1661f92b273bbef3c881caab3e1
simz007 <51645428+simz007@users.noreply.github.com>
2019-10-30 11:11:17 -0400

    Ex06: zebra was missing the y value

--- exercices/exercice06/js/script.js
+++ exercices/exercice06/js/script.js
@@ -27,7 +27,7 @@ function setup() { ////FIXED : missing space
   createCanvas(windowWidth, windowHeight);
   tiger = new Predator(100, 100 , 100, 5, color(200, 200, 0), 40);////FIXED missing y value
   antelope = new Prey(100, 100, 10, color(255, 100, 10), 50);
-  zebra = new Prey(100, 8, color(255, 255, 255), 60);
+  zebra = new Prey(100,100, 8, color(255, 255, 255), 60);////FIXED missing y value
   bee = new Prey(100, 100, 20, color(255, 255, 0), 10);
 }
 
